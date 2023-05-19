8d8891bb9b16b5854f2d3663fb434f5695ef5ca6
Alexander <akrimec@gmail.com>
2023-05-19 15:24:23 +0300

    functions done

---
+++ JavaScript basics/25. Functions, arrow functions (ES6)/functions.js
@@ -0,0 +1,44 @@
+'use strict';
+
+let num = 20;
+function showFirstMassage(text) {
+    console.log(text);
+    let num = 10;
+    console.log(num)
+}
+
+showFirstMassage('Hello World!');
+console.log(num);
+
+function calc(a, b) {
+    return a + b;
+    console.log('blablabla'); // unreachable code
+}
+console.log(calc(4, 3))
+console.log(calc(5, 6))
+console.log(calc(10, 6))
+
+function ret() {
+    let num = 50;
+    return num;
+}
+
+const  anotherNum = ret();
+console.log(anotherNum);
+
+const logger = function () {
+    console.log('Hello');
+}; // !!!!
+
+logger();
+
+
+const cal = (a, b) => a + b; // arrow function
+
+function  convert (amount, curr) {
+    console.log(curr * amount);
+    return curr * amount;
+}
+
+convert(500, 28);
+convert(500, 32);
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 ![2.png](JavaScript%20basics/11.%20Data%20type%20classification%20in%20JavaScript/types_2020.jpg)
 ### Conditions & Loops in JavaScript
 ![1.jpg](JavaScript%20basics/22.%20Loops/Conditions%20&%20Loops.jpg)
+### Functions, arrow functions (ES6)
+![1.jpg](JavaScript%20basics/25.%20Functions,%20arrow%20functions%20(ES6)/functions.jpg)
