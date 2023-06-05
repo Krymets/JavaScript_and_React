f07c73ca842eabfd89387da7fb95e1ab5b4348ee
Alexander <akrimec@gmail.com>
2023-06-05 12:43:06 +0300

    breakpoints +

---
+++ JavaScript basics/38. Console.Breakpoints/console.js
@@ -0,0 +1,22 @@
+'use strict';
+
+function hello() {
+    console.log('Hello World');
+}
+
+hello();
+
+function hi() {
+    console.log('Say hi!');
+}
+
+hi();
+
+const arr = [1, 14, 4, 30, 54],
+    sorted = arr.sort(compareNum);
+
+function compareNum(a, b) {
+    return a - b;
+}
+
+console.log(sorted);
