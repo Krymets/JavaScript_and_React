8aca8fe46c98b41bbfe0e67a93eeef027755d3d3
Alexander <akrimec@gmail.com>
2023-07-12 14:29:17 +0300

    test react

--- React basics/my-app/src/App.js
+++ React basics/my-app/src/App.js
@@ -1,25 +1,83 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {Component, StrictMode} from "react";
+
+
+const Header = () => {
+  return <h2>Hello World!</h2>
+}
+
+// const Field = () => {
+//   const holder = 'Enter here';
+//   const styledField = {
+//     width: '300px'
+//   };
+//   return <input
+//           placeholder={holder}
+//           type="text"
+//           style={styledField}/>
+// }
+
+class Field extends Component {
+    render() {
+        const holder = 'Enter here';
+        const styledField = {width: '300px'
+        };
+        return <input
+              placeholder={holder}
+              type="text"
+              style={styledField}/>
+    }
+}
+
+function Btn() {
+  const text = 'Log in';
+  const logged = false;
+  // const res = () => {
+  //   return 'Log in';
+  // }
+  // const p = <p>LOG IN</p>
+  return <button>{logged ? 'Enter' : text}</button>
+}
+
+function WhoAmI (props) {
+    return (
+        <div>
+            <h1>My name is {props.name()}, surname - {props.surname}</h1>
+            <a href={props.link}>My profile</a>
+        </div>
+    )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WhoAmI name={() => {return "John"}} surname="Smith" link="facebook.com"/>
+      {/*<WhoAmI name={{firstName: "Alex"}} surname="Shepard" link="facebook.com"/>*/}
+      <Header></Header>
+      <StrictMode>
+          <Header/>
+      </StrictMode>
+      <Field/>
+      <Btn/>
+      {/*<header className="App-header">*/}
+      {/*  <img src={logo} className="App-logo" alt="logo" />*/}
+      {/*  <p>*/}
+      {/*    Edit <code>src/App.js</code> and save to reload.*/}
+      {/*  </p>*/}
+      {/*  <a*/}
+      {/*    className="App-link"*/}
+      {/*    href="https://reactjs.org"*/}
+      {/*    target="_blank"*/}
+      {/*    rel="noopener noreferrer"*/}
+      {/*  >*/}
+      {/*    Learn React*/}
+      {/*  </a>*/}
+      {/*</header>*/}
     </div>
   );
 }
 
+export {Header};
 export default App;
--- React basics/my-app/src/index.js
+++ React basics/my-app/src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import  React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {Header} from "./App";
+
+// import reportWebVitals from './reportWebVitals';
+
+// const elem = <h2>Hello World!</h2>;
+// const elem = React.createElement('h2', {className: 'greetings'},
+//     'Hello World!')
+
+// const text = 'Hello World!'
+// const elem = (
+//     <div>
+//         <h2 className="text">Текст: {text}</h2>
+//         <h2>{2 + 2}</h2>
+//         <h2>{['222', 'dsdf']}</h2>
+//         <label htmlFor=""></label>
+//         <input type="text" />
+//         <button tabIndex="0">Click</button>
+//         <button/>
+//     </div>
+// );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//     elem
+// );
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
