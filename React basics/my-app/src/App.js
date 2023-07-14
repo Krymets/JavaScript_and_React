9ee2a981019695877dbb2ee4b3a4d9e76acb0f14
Alexander <akrimec@gmail.com>
2023-07-14 14:47:15 +0300

    test react 2

--- React basics/my-app/src/App.js
+++ React basics/my-app/src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 import React from "react";
 import {Component, StrictMode} from "react";
 
-
-const Header = () => {
-  return <h2>Hello World!</h2>
-}
+const Header = () => <h2>Hello World!</h2>;
+// const Header = () => {
+//   return <h2>Hello World!</h2>
+// }
 
 // const Field = () => {
 //   const holder = 'Enter here';
@@ -41,20 +41,73 @@ function Btn() {
   return <button>{logged ? 'Enter' : text}</button>
 }
 
-function WhoAmI (props) {
-    return (
-        <div>
-            <h1>My name is {props.name()}, surname - {props.surname}</h1>
-            <a href={props.link}>My profile</a>
-        </div>
-    )
+// function WhoAmI (props) {
+//     return (
+//         <div>
+//             <h1>My name is {props.name()}, surname - {props.surname}</h1>
+//             <a href={props.link}>My profile</a>
+//         </div>
+//     )
+// }
+
+
+class WhoAmI extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            years: 27,
+            text: '+++',
+            position: ''
+        }
+        // this.nextYear = this.nextYear.bind(this)
+    }
+
+    // nextYear = () => {
+    //     console.log('+++');
+    //     this.setState({
+    //         years: this.state.years + 1
+    //     })
+    // }
+
+    nextYear = () => {
+        console.log('+++');
+        this.setState(state => ({
+            years: state.years + 1
+        }))
+    }
+
+    commitInputChanges = (e,color) => {
+        console.log(color)
+        this.setState({
+            position: e.target.value
+        })
+    }
+
+    render() {
+        const {name, surname, link} = this.props;
+        const {position, years} = this.state;
+        return (
+            <div>
+                <button onClick={this.nextYear}>{this.state.text}</button>
+                <h1>My name is {name}, surname - {surname},
+                    age - {years},
+                    position = {position}</h1>
+                <a href={link}>My profile</a>
+                <form>
+                    <span>Enter position</span>
+                    <input type="text"
+                           onChange={(e) => this.commitInputChanges(e, 'some color')}/>
+                </form>
+            </div>
+        )
+    }
 }
 
 function App() {
   return (
     <div className="App">
-      <WhoAmI name={() => {return "John"}} surname="Smith" link="facebook.com"/>
-      {/*<WhoAmI name={{firstName: "Alex"}} surname="Shepard" link="facebook.com"/>*/}
+      <WhoAmI name="John" surname="Smith" link="facebook.com"/>
+      <WhoAmI name="Alex" surname="Shepard" link="facebook.com"/>
       <Header></Header>
       <StrictMode>
           <Header/>
